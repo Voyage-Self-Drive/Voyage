5c6b4dc3f6eb9f609817cff07ee56c47118dcb30
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-06-18 11:00:03 +0200

    Update Lanes.py

--- Lanes.py
+++ Lanes.py
@@ -11,12 +11,23 @@ def Canny(video):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def region_of_interest(video):
+    height = video.shape[0]
+    polygons = np.array([
+        [(300, height), (1800,height), (800, 350)]
+        ])
+    mask = np.zeros_like(video)
+    cv2.fillPoly(mask, polygons, (255,255,255))
+    masked_video = cv2.bitwise_and(video, mask)
+    return masked_video
+
 
 while cap.isOpened():
     ret, frame = cap.read()
     canny = Canny(frame)
+    cropped_video = region_of_interest(canny)
     
-    cv2.imshow('lanes',canny)
+    cv2.imshow('lanes',cropped_video)
     if cv2.waitKey(30) & 0xFF == ord('q')or 0xFF == ord('Q'):
         break
 
