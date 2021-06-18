482d3037db128c2ecd03623469d2c9e4d0a38d6b
Sven Skyth Henriksen <sven.skyth.henriksen@gmail.com>
2021-06-18 11:56:02 +0200

    Update Lanes.py

--- Lanes.py
+++ Lanes.py
@@ -1,9 +1,41 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from numpy.lib.function_base import average
 
 
 cap = cv2.VideoCapture('cars2.mp4')
+
+
+def make_coordinates( video, line_parameters):
+    slope, intercept = line_parameters
+    y1 = video.shape[0]
+    y2 = int(y1* (3/5))
+    x1 = ((y1 - intercept) / slope)
+    x2 = ((y2 - intercept) / slope)
+    return np.array([x1,y1,x2,y2])
+
+
+
+def average_slope_intercept(video, lines):
+    left_fit=[]
+    right_fit=[]
+    for line in lines: 
+        x1,y1,x2,y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+        
+    left_fit_average = np.average(left_fit, axis = 0)
+    right_fit_average = np.average( right_fit, axis= 0)
+    left_line = make_coordinates(video, left_fit_average)
+    right_line = make_coordinates(video, right_fit_average)
+    return np.array([left_line, right_line])
+
 
 def Canny(video):
     gray = cv2.cvtColor(video, cv2.COLOR_RGB2GRAY)
@@ -12,12 +44,12 @@ def Canny(video):
     return canny
 
 def display_lines(video, lines):
-    frame = np.zeros_like(video)
+    line_video = np.zeros_like(video)
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
-            cv2.line(frame, (x1, y1), (x2, y2), (255,0,0), 10)
-    return frame
+        for x1, y1, x2, y2  in lines:
+            print(x1, y1 ,x2, y2)
+            cv2.line(line_video, (x1, y1), (x2, y2), (255,0,0), 10)
+    return line_video
 
 def region_of_interest(video):
     height = video.shape[0]
@@ -32,12 +64,15 @@ def region_of_interest(video):
 
 while cap.isOpened():
     ret, frame = cap.read()
-    canny = Canny(frame)
-    cropped_video = region_of_interest(canny)
+    canny_image = Canny(frame)
+    cropped_video = region_of_interest(canny_image)
     lines = cv2.HoughLinesP(cropped_video, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap= 5)
-    line_video = display_lines(frame, lines)
+    averaged_lines = average_slope_intercept(frame, lines)
+    line_video = display_lines(frame, averaged_lines)
     combo_video = cv2.addWeighted(frame, 0.8, line_video, 1, 1)
-    cv2.imshow('lanes',combo_video)
+
+
+    cv2.imshow('lanes', combo_video)
     if cv2.waitKey(30) & 0xFF == ord('q')or 0xFF == ord('Q'):
         break
 
